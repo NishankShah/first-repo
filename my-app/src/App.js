0d9b4652668feae72404e52995998fbd0beaf256
Nishank <darknishank@gmail.com>
2023-11-27 10:40:35 +0545

    second

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,23 @@
-
+import React from 'react';
 import './App.css';
+import Footer from './Footer';
+import Nav from './Nav';
+import Heading from "./Heading";
+import Main from "./Main";
+
 
 function App() {
-  return (
-    <div className="App">
-      homepage
-    </div>
-  );
-}
+  return (
+    <div className="App">
+      <Heading />
+     
+      <Nav />
 
+      <Main />
+      
+      <Footer />
+    </div>
+  );
+}
+ 
 export default App;
---
+++ my-app/src/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+    <h1>This is an h1 nav bar</h1>
+<img src="" alt="logo if not added" />
+<ul>
+    <li><a>Home</a></li>
+    <li><a>About</a></li>
+    <li><a>menu</a></li>
+    <li><a>checkout</a></li>
+</ul>
+</>
+  );
+}
+
+export default Nav;
